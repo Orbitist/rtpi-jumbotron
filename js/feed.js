752b673a765c553385ced0c5ac4845dfe7149fca
Nicholas Gunner <gunnernr@gmail.com>
2016-11-08 11:50:15 -0500

    added masonry

--- js/feed.js
+++ js/feed.js
@@ -14,8 +14,30 @@ var contentFeed = (function () {
 
 function renderFeedItems() {
   for (var i = 0; i < contentFeed.length; i++) {
-    $('div.items').append('<p>' + contentFeed[i].title + '</p>');
+    if (contentFeed[i].field_featured == 'true') {
+      $('div.featured-grid').append('<p>' + contentFeed[i].title + '</p>');
+    } else {
+      // Normal Grid
+      $('div.grid').append(
+        '<div class="grid-item">' +
+          '<img class="grid-image" src="' + contentFeed[i].field_thumbnail + '" />' +
+          '<div class="grid-text">' +
+            '<div class="grid-title">' + contentFeed[i].title + '</div>' +
+            '<div class="grid-description"' + contentFeed[i].field_description + '</div>' +
+          '</div>' +
+        '</div>'
+      );
+    }
   }
 };
 
 renderFeedItems();
+
+
+var $grid = $('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
