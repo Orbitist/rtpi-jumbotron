05431ac0456845f420a1f6b2e3e4adb2cd169867
Nicholas Gunner <gunnernr@gmail.com>
2016-11-08 16:02:51 -0500

    css tweak

--- js/feed.js
+++ js/feed.js
@@ -14,16 +14,40 @@ var contentFeed = (function () {
 
 function renderFeedItems() {
   for (var i = 0; i < contentFeed.length; i++) {
+
+    var videoEmbed = contentFeed[i].field_video_embed_code;
+    if (contentFeed[i].field_video_embed_code.length > 3) {
+      videoEmbed = JSON.parse(contentFeed[i].field_video_embed_code);
+    }
+
     if (contentFeed[i].field_featured == 'true') {
       $('div.featured-grid').append('<p>' + contentFeed[i].title + '</p>');
     } else {
-      // Normal Grid
       $('div.grid').append(
-        '<div class="grid-item">' +
+        '<div class="grid-item" data-toggle="modal" data-target=".item' + contentFeed[i].nid + '">' +
           '<img class="grid-image" src="' + contentFeed[i].field_thumbnail + '" />' +
           '<div class="grid-text">' +
             '<div class="grid-title">' + contentFeed[i].title + '</div>' +
             '<div class="grid-description"' + contentFeed[i].field_description + '</div>' +
+          '</div>' +
+        '</div>' +
+        '<div class="modal fade item' + contentFeed[i].nid + '" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">' +
+          '<div class="modal-dialog modal-lg" role="document">' +
+            '<div class="modal-content">' +
+              '<div class="modal-header">' +
+                '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+                '<h4 class="modal-title" id="myModalLabel">Modal title</h4>' +
+              '</div>' +
+              '<div class="modal-body">' +
+
+                videoEmbed +
+
+
+              '</div>' +
+              '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+              '</div>' +
+            '</div>' +
           '</div>' +
         '</div>'
       );
@@ -35,7 +59,7 @@ renderFeedItems();
 
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
-  columnWidth: 200,
+  fitWidth: true,
   gutter: 5
 });
 $grid.imagesLoaded().progress( function() {
