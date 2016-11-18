1032a91527520d65136e6ddf88c8963338ee5238
Nicholas Gunner <gunnernr@gmail.com>
2016-11-18 10:00:35 -0500

    front end tweaks

--- js/feed.js
+++ js/feed.js
@@ -44,16 +44,13 @@ function renderFeedItems() {
           '<div class="modal-content">' +
             '<div class="modal-header">' +
               '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-              '<h4 class="modal-title" id="myModalLabel">Modal title</h4>' +
+              '<p class="modal-title lead" id="myModalLabel">' + contentFeed[i].title + '</p>' +
             '</div>' +
             '<div class="modal-body">' +
 
               videoEmbed +
               mapEmbed +
 
-            '</div>' +
-            '<div class="modal-footer">' +
-              '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
             '</div>' +
           '</div>' +
         '</div>' +
