3d256fe84471d4f071bc07b3c86f5a385731a89d
Nicholas Gunner <gunnernr@gmail.com>
2016-11-28 06:54:24 -0500

    added sounds loop

---
+++ js/sounds.js
@@ -0,0 +1,14 @@
+var soundClips = ['sound/clips/c.mp3','sound/clips/e.mp3','sound/clips/g.mp3','sound/clips/b.mp3','sound/clips/chord.mp3'];
+
+var soundTrack = 0;
+
+$( "body" ).click(function() {
+  if (soundTrack == 5){
+    soundTrack = 0
+  };
+  var sound = new Howl({
+    src: [soundClips[soundTrack]]
+  });
+  sound.play();
+  soundTrack = soundTrack + 1;
+});
