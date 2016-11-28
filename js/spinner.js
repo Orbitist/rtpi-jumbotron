a17bdd1022157e9c4082a4286e5ef47a20627ce1
Nicholas Gunner <gunnernr@gmail.com>
2016-11-28 06:55:41 -0500

    update to the spinner

--- js/spinner.js
+++ js/spinner.js
@@ -1,5 +1,5 @@
 function removeSpinner() {
-  $(document).find(".spinner").delay(2000).fadeOut(500, function () {
+  $(document).find(".spinner").delay(2000).fadeOut(1000, function () {
     $(this).remove();
   });
 };
