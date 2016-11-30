97c714c54149f767c8a7ebb771996b3992ab871c
Nicholas Gunner <gunnernr@gmail.com>
2016-11-30 17:55:08 -0500

    added welcome section

--- js/filters.js
+++ js/filters.js
@@ -20,3 +20,20 @@ function filterGrid(cat) {
     $( ".grid-cat-Art" ).css('opacity', '1');
   }
 };
+
+
+// Smooth Scrolling
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
